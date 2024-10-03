6ee71ba1b24da3d21435fe5d0dd5d431e9e3d198
Vinoth R <158076208+Vinothofficial21@users.noreply.github.com>
2024-10-03 21:53:45 +0530

    Add files via upload

---
+++ src/delux.java
@@ -0,0 +1,18 @@
+public class delux extends Pizza{
+
+    public delux(Boolean veg) {
+        super(veg);
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addExtraCheese() {
+
+    }
+
+    @Override
+    public void addExtraToppings() {
+
+    }
+}
